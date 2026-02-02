608c637aea01daa2081d104bff78efa8d7748880
Angel <agaleaa02@iesalbarregas.es>
2026-02-02 17:18:49 +0100

    que se ve el logo, que no se vía

--- ClickPermiso/src/Componentes/Header.tsx
+++ ClickPermiso/src/Componentes/Header.tsx
@@ -1,5 +1,10 @@
+import logo_albarregas from "../assets/logo_albarregas.png"
+
 const Header = () => (
   <div className="bg-white px-8 py-4 border-b border-gray-200 flex justify-between items-center sticky top-0 z-50">
+    <div>
+      <img src={logo_albarregas} alt="Logo" className=" w-50 h-30"/>
+    </div>
     <div className="text-gray-700 font-semibold text-lg">
       I.E.S Albarregas
     </div>
